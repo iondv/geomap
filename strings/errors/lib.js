f16d14ea6e27d5ea74d42d2b1de1d3af1fa6c22c
sovereign <allys@live.ru>
2020-11-26 11:48:17 +1000

    IONCORE 648 strings translation

--- strings/errors/lib.js
+++ strings/errors/lib.js
@@ -2,7 +2,7 @@ const codes = require('../../errors/lib');
 const {w: t} = require('core/i18n');
 
 module.exports = {
-  [codes.NO_DS]: t('Не настроен источник данных словаря координат.'),
-  [codes.WRONG_QUERY]: t('Некорректный объект описания данных слоя.'),
+  [codes.NO_DS]: t('Data source for coordinate dictionary is not set.'),
+  [codes.WRONG_QUERY]: t('Incorrect layer data description object.'),
   [codes.DENIED]: t('Access denied!')
 };
--- strings/errors/web-errors.js
+++ strings/errors/web-errors.js
@@ -2,9 +2,9 @@ const codes = require('../../errors/web-errors');
 const {w: t} = require('core/i18n');
 
 module.exports = {
-  [codes.RENDER_FAIL]: t('Ошибка рендера шаблона %template'),
-  [codes.NO_LAYER]: t('Ошибка получения объекта'),
-  [codes.NO_LAYERS]: t('Ошибка получения объектов'),
-  [codes.LAYERS_404]: t('Объекты не найдены'),
-  [codes.NO_REPO]: t('Не указан репозиторий метаданных гео-модуля.')
+  [codes.RENDER_FAIL]: t('Template %template rendering error'),
+  [codes.NO_LAYER]: t('Object reading error'),
+  [codes.NO_LAYERS]: t('Objects reading error'),
+  [codes.LAYERS_404]: t('Objects were not found'),
+  [codes.NO_REPO]: t('Geomap metadata repository is not set.')
 };
